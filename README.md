8fa30eab87f2328819d877f0e48417548be8db29
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-04-26 20:05:43 +0530

    Node and Mongodb connection is successfully connected Done

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+git clone https://github.com/bhagatbhutale/CRUDperform.git
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+// require mongoose
 const mongoose = require("mongoose");
 
-
+// connect to the local server
 mongoose.connect("mongodb://127.0.0.1:27017");
-
+// storing connection in db
 const db =  mongoose.connection;
-
+// check connection success
 db.on("open", () => {
     console.log("Connected to MongoDb database Successfully...");
 })
-
+// check connection error occruu
 db.on("error", () => {
     console.log("Connection Not Successful !")
 } )
+// -------------------------------------------------------------------
